38e48ca281e8b0db18bc3e75d42d3acf334fc057
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-09-22 12:39:43 +0200

    fix Feedback component

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,35 +2,24 @@ import React, { Component } from 'react';
 
 export default class Feedback extends Component {
   render() {
+    const { handelBtnClick, data } = this.props;
     return (
       <section>
         <h1>Please leave feedback</h1>
 
         <ul>
           <li>
-            <button
-              type="button"
-              name="good"
-              onClick={this.props.handelBtnClick}
-            >
+            <button type="button" name="good" onClick={handelBtnClick}>
               Good
             </button>
           </li>
           <li>
-            <button
-              type="button"
-              name="neutral"
-              onClick={this.props.handelBtnClick}
-            >
+            <button type="button" name="neutral" onClick={handelBtnClick}>
               Neutral
             </button>
           </li>
           <li>
-            <button
-              type="button"
-              name="bad"
-              onClick={this.props.handelBtnClick}
-            >
+            <button type="button" name="bad" onClick={handelBtnClick}>
               bad
             </button>
           </li>
@@ -39,19 +28,19 @@ export default class Feedback extends Component {
         <h2>Statistics</h2>
         <ul>
           <li>
-            <p>Good: {this.props.data.good} </p>
+            <p>Good: {data.good} </p>
           </li>
           <li>
-            <p>Neutral: {this.props.data.neutral}</p>
+            <p>Neutral: {data.neutral}</p>
           </li>
           <li>
-            <p>Bad: {this.props.data.bad}</p>
+            <p>Bad: {data.bad}</p>
           </li>
           <li>
-            <p>Total: {this.props.data.total}</p>
+            <p>Total: {data.total}</p>
           </li>
           <li>
-            <p>Positive feedback: {`${this.props.data.positiveFeedback}%`}</p>
+            <p>Positive feedback: {`${data.positiveFeedback}%`}</p>
           </li>
         </ul>
       </section>
